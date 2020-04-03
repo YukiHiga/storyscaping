fe80cb8cfe6b07e38d709d5422a65e70c77d1043
YukiHiga <yukihigashidani@hotmail.com>
2020-04-03 20:03:25 +0200

    opdateret kommentarer
    kommentarer i index.php

--- wp-content/plugins/newsletter-popup/index.php
+++ wp-content/plugins/newsletter-popup/index.php
@@ -36,10 +36,10 @@ function newsletter_form()
     return $content;
     
 }
-    #Første parameter er et unikt navn for short-code for newsletter popup form. Den anden parameter er navnet på funktionen - newsletter_popup_form
+    #Første parameter er et unikt navn for short-code for newsletter popup. Den anden parameter er navnet på funktionen - newsletter_form
     add_shortcode('show_popup_newsletter','newsletter_form');
 
-    #Action udfører wp_enqueue_scripts med funktionen register_styles_and_scripts_for_responsive_discount_popup_plugin, så det kan aktiveres
+    #Action udfører wp_enqueue_scripts med funktionen register_styles_and_scripts_for_newsletter_popup_plugin, så det kan aktiveres
     add_action('wp_enqueue_scripts','register_styles_and_scripts_for_plugin');
 
 #Linker stylesheets og script filer
